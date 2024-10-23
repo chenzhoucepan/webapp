eb070aab3b35dfe95b8e01af633fe66889be4dae
woody <wondye@qq.com>
2024-10-23 08:34:39 +0800

    te

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -19,50 +19,50 @@ import SupportIcon from './icons/IconSupport.vue'
     provides you with all information you need to get started.
   </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
+<!--  <WelcomeItem>-->
+<!--    <template #icon>-->
+<!--      <ToolingIcon />-->
+<!--    </template>-->
+<!--    <template #heading>Tooling</template>-->
 
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener">Cypress Component Testing</a
-    >.
+<!--    <a href="https://on.cypress.io/component" target="_blank" rel="noopener">Cypress Component Testing</a-->
+<!--    >.-->
 
-    <br />
+<!--    <br />-->
 
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
+<!--    More instructions are available in <code>README.md</code>.-->
+<!--  </WelcomeItem>-->
 
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
+<!--  <WelcomeItem>-->
+<!--    <template #icon>-->
+<!--      <EcosystemIcon />-->
+<!--    </template>-->
+<!--    <template #heading>Ecosystem</template>-->
 
-    Get official tools and libraries for your project:
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
+<!--    Get official tools and libraries for your project:-->
+<!--    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>-->
+<!--    a visit.-->
+<!--  </WelcomeItem>-->
 
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
+<!--  <WelcomeItem>-->
+<!--    <template #icon>-->
+<!--      <CommunityIcon />-->
+<!--    </template>-->
+<!--    <template #heading>Community</template>-->
 
-    Got stuck? Ask your question on
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@js</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
+<!--    Got stuck? Ask your question on-->
+<!--    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@js</a>-->
+<!--    twitter account for latest news in the Vue world.-->
+<!--  </WelcomeItem>-->
 
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
+<!--  <WelcomeItem>-->
+<!--    <template #icon>-->
+<!--      <SupportIcon />-->
+<!--    </template>-->
+<!--    <template #heading>Support Vue</template>-->
 
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+<!--    As an independent project, Vue relies on community backing for its sustainability. You can help-->
+<!--    us by-->
+<!--    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.-->
+<!--  </WelcomeItem>-->
 </template>
