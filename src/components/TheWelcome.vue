e034d416e2f6eef085ad30315c8e93a37a9083af
woody <wondye@qq.com>
2024-10-23 08:36:00 +0800

    tt

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,11 +12,6 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
   </WelcomeItem>
 
 <!--  <WelcomeItem>-->
